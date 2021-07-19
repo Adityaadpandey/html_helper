83d13acbe095a9c4c0ec85a8d42f428d294e258c
aditya <adityapandeyadp@gmail.com>
2021-07-19 11:23:37 +0530

    commit on main

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hi")
+import os
+
+#os.system("tree")
+get = input("what is text-- ")
+
+f= open("test.txt","w+")
+f.write(get)
+
+f.close() 
+
+os.system("python -m texttohtml.convert test.txt -o test.html")
+
